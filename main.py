6f25cd8086237cf08f629347921425fcf7a22631
WTL04 <williamluu04@gmail.com>
2026-04-18 19:03:43 -0700

    adding socket 5 sec health check

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+import socketio
+import asyncio
 from fastapi import FastAPI
 
-app = FastAPI(title="fullypack_fastapi")
+# async server
+sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
+
+# fastapi server
+app = FastAPI(title="fullyhacks")
+
+# wrapping fastapi with socketio asgi application
+socket_app = socketio.ASGIApp(sio, app)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Welcome to the fullypack_fastapi!"}
+@sio.event
+async def connect(sid, environ):
+    print(f"Client connected: {sid}")
+
+
+async def health_broadcast():
+    while True:
+        await sio.emit("health", {"status": "ok", "tick": "server alive"})
+        await asyncio.sleep(5)
+
+
+@app.on_event("startup")
+async def startup():
+    asyncio.create_task(health_broadcast())
