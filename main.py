1a67dfb63f0d205725b9ba1c22c7eb73831efd76
WTL04 <williamluu04@gmail.com>
2026-04-18 18:47:57 -0700

    fixed main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from fastapi import Depends, FastAPI
-from app.api.v1 import router as api_v1_router
+from fastapi import FastAPI
 
 app = FastAPI(title="fullypack_fastapi")
-
-app.include_router(api_v1_router, prefix="/api/v1")
 
 
 @app.get("/")
